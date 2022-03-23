9bba8c0b61701801b42150e820a61caf3099ea83
Fariya <98416116+FariyaSultanaPrity@users.noreply.github.com>
2022-03-23 10:56:17 +0600

    Add files via upload

---
+++ InsertDBData/view/Registration.php
@@ -0,0 +1,24 @@
+<?php
+include ("../control/RegistrationCheck.php")
+?>
+
+
+<html>
+<body>
+<form action="" method="post">  
+
+First Name:
+<input type="text" name="fname"> </br>
+Last Name:
+<input type="text" name="lname"> </br>
+Age:
+<input type="number" name="age"> </br>
+Salary:
+<input type="number" name="salary"> </br>
+
+<input type="submit" name="Submit"> </br>
+</form>
+
+</body>
+
+</html>
